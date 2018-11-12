13c9d70c3e049e854c6e7b4bda66950cf732cc4d
Kacper Ostrowski <kacper.j.ostrowski@gmail.com>
2018-11-12 21:45:03 +0100

    init

---
+++ property-browser/src/main/java/pl/ostrowski/apigatewaydemo/properties/propertybrowser/PropertyBrowserApplication.java
@@ -0,0 +1,12 @@
+package pl.ostrowski.apigatewaydemo.properties.propertybrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyBrowserApplication.class, args);
+	}
+}
